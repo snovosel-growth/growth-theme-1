9ff2a28ab1a3373916f3f5d1a23bc83ab0d7c0dd
snovosel-growth <steve@growthgenerator.co>
2023-08-19 15:10:56 -0400

    expand featured product section

--- webpack/section-loader.js
+++ webpack/section-loader.js
@@ -4,7 +4,39 @@ import fs from 'fs';
 function resolveSectionSettings(settings) {
     return settings.map(setting => {
         const filePath = path.resolve('src', 'settings', `${setting}.json`);
-        return JSON.parse(fs.readFileSync(filePath));
+        let blockData;
+
+        if (typeof setting === 'string') {
+            blockData = JSON.parse(fs.readFileSync(filePath));
+        } else {
+            blockData = setting;
+        }
+
+        return blockData;
+    });
+}
+
+function resolveSectionBlocks(blocks) {
+    return blocks.map(block => {
+        try {
+            const filePath = path.resolve('src', 'blocks', `${block}.json`);
+
+            let blockData;
+
+            if (typeof block === 'string') {
+                blockData = JSON.parse(fs.readFileSync(filePath));
+            } else {
+                blockData = block;
+            }
+
+            if (blockData.settings) {
+                blockData.settings = resolveSectionSettings(blockData.settings);
+            }
+
+            return blockData;
+        } catch (error) {
+            console.log('error resolveSectionBlocks', block);
+        }
     });
 }
 
@@ -17,6 +49,6 @@ export default function (source) {
         tag: section.tag,
         disabled_on: section.disabled_on,
         settings: resolveSectionSettings(section.settings),
-        blocks: section.blocks
+        blocks: resolveSectionBlocks(section.blocks)
     }, null, "\t")}\n{% endschema %}`;
 }
